d4bb2b72fd49031d6452ccf69338b2825b3b551a
Hriday Barot <hridaybarot1@gmail.com>
2026-01-12 20:20:47 +0100

    Switched some types from class to structs

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ public static class Program
         Camera cam = new Camera();
 
         cam.aspect_ratio = 16.0/9.0;
-        cam.image_width = 400;
+        cam.image_width = 1080;
         cam.samples_per_pixel = 100;
         cam.max_depth=50;
         cam.render(world);
